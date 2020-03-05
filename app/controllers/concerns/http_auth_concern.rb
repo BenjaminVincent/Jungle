290bd0c6b0ebb586bdafa43d21425a7b8aac3527
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-03-05 00:50:40 +0000

    admin auth added

---
+++ app/controllers/concerns/http_auth_concern.rb
@@ -0,0 +1,12 @@
+# module HttpAuthConcern  
+#   extend ActiveSupport::Concern
+#   included do
+#       before_action :http_authenticate
+#   end
+#   def http_authenticate
+#       return true unless Rails.env == 'production'
+#       authenticate_or_request_with_http_basic do |username, password|
+#           username == ENV["HTTP_BASIC_USER"] && password == ENV["HTTP_BASIC_PASSWORD"]
+#       end
+#   end
+# end
