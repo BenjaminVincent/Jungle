5bd31f7f79568b851e5885e7fa98752d075da161
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-03-10 01:20:25 +0000

    finished writing product tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ActiveRecord::Base
-<<<<<<< HEAD
-=======
   validates :email, uniqueness: true
 
   has_secure_password
   
->>>>>>> feature/user-auth
 end
+
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    it "validates n with a name"
+    it "validates n with a name" do
     @category = Category.new(name: "fish")
     @product = Product.new(name: "salmon", price: 999, quantity: 1, category: @category)
       expect(@product).to be_valid
@@ -11,27 +11,26 @@ RSpec.describe Product, type: :model do
       @category = Category.new(name: "fish")
       @product = Product.new(name: nil, price: 999, quantity: 1, category: @category)
       expect(@product).to be_invalid
-      expect(@product.errors.full_messages).to contain_exactly("name cannot be nil")
+      expect(@product.errors.full_messages).to contain_exactly("Name can't be blank")
     end
     it "throws an error when price is nil" do
       @category = Category.new(name: "fish")
       @product = Product.new(name: "salmon", price: nil, quantity: 1, category: @category)
       expect(@product).to be_invalid
-      expect(@product.errors.full_messages).to contain_exactly("price cannot be nil")
+      expect(@product.errors.full_messages).to contain_exactly("Price can't be blank", "Price cents is not a number", "Price is not a number")
 
     end
     it "throws an error when quantity is nil." do
       @category = Category.new(name: "fish")
       @product = Product.new(name: "salmon", price: 999, quantity: nil, category: @category)
       expect(@product).to be_invalid
-      expect(@product.errors.full_messages).to contain_exactly("quantity cannot be nil") 
+      expect(@product.errors.full_messages).to contain_exactly("Quantity can't be blank") 
     end
     it "throws an error when category is nil" do
       @category = Category.new(name: "fish")
       @product = Product.new(name: "salmon", price: 999, quantity: 1, category: nil)
       expect(@product).to be_invalid
-      expect(@product.errors.full_messages).to contain_exactly("category cannot be nil")
+      expect(@product.errors.full_messages).to contain_exactly("Category can't be blank")
     end
   end
 end
-
